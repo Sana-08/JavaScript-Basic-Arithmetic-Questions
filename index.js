d16be6dddef4463ace852ce02b2b55cd61a8d866
Sana-08 <sana22@navgurukul.org>
2024-03-24 15:43:54 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,110 @@
+// QUESTION NUMBER 1
+// Take two numbers a,b and print the sum, difference, product, quotient and modulus of a & b.
+// let a=6;
+// let b=4;
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+
+
+// QUESTION NUMBER 2
+// Take two numbers and only print the integer part when a is divided by b.
+// let a=10;
+// let b=3;
+// console.log(Math.floor(a%b))
+
+
+// QUESTION NUMBER 3
+// Take two numbers a,b and print the exponential power of the first number raised by the second.
+// let a=5;
+// let b=3;
+// console.log(a**b)
+
+
+// QUESTION NUMBER 4
+// Take two numbers a,b and only print the decimal part of the result obtained when a is divided by b.
+// let a=56;
+// let b=1900;
+// let x=a%b
+// console.log(x/b)
+
+
+// QUESTION NUMBER 5
+// Take marks in 5 subjects A,B,C,D,E and print out the average of marks from five subject marks.
+// let a=89;
+// let b=78;
+// let c=69;
+// let d=90;
+// let e=88;
+// let total=a+b+c+d+e;
+// console.log(total/5)
+
+
+// QUESTION NUMBER 6
+// Take the cost price a and selling price b of a product, and print the profit obtained on the product.
+// let a=80;
+// let b=95;
+// let profit=b-a;
+// console.log(profit)
+
+
+// QUESTION NUMBER 7
+// Take a number and print out the last digit of the number.
+// let a=786;
+// let last_digit=a%10;
+// console.log(last_digit)
+
+
+// QUESTION NUMBER 8
+// Take a 2 digit number and return the first and last digits.
+// let a=45;
+// let last=a%10
+// let first=parseInt(a/10)
+// console.log(first, last)
+
+
+// QUESTION NUMBER 9
+// Write a JavaScript program to find the largest of three numbers.
+// let x=8;
+// let y=31;
+// let z=6;
+// if (x>y && x>z){
+//     console.log("x is largest")
+// }
+// else if (y>z && y>x){
+//     console.log("y is largest")
+// }
+// else{
+//     console.log("z is largest")
+// }
+
+
+// QUESTION NUMBER 10
+// Write a JavaScript program to check whether a number is even or odd.
+// let x=75;
+// if (x%2==0){
+//     console.log("even")
+// }
+// else{
+//     console.log("odd")
+// }
+
+
+// QUESTION NUMBER 12
+// Write a JavaScript program to check whether a given number is prime or not.
+
+
+
+// QUESTION NUMBER 12
+//  Write a JavaScript program to convert temperature from Celsius to Fahrenheit and vice versa.
+// Convert Celsius to Fahrenheit
+// let Celsius1=35
+// let Fahrenheit1=(Celsius1*9/5)+32
+// console.log(Celsius1+Fahrenheit1)
+
+// Convert Fahrenheit to Celsius
+// let Fahrenheit2=87;
+// let Celsius2=(Fahrenheit2-32)*5/9
+// console.log(Fahrenheit2+Celsius2)
